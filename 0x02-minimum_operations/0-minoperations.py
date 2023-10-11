3d24a35c74e6e8d09ec177ed0a78408ce32db901
Anteneh Mulusew <85405803+muluante@users.noreply.github.com>
2023-10-11 09:26:57 +0300

    Update 0-minoperations.py
    new commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-""" Minimum Operations function """
-import math
+"""Minimum Operations"""
+
+
+def primeFactorization(x):
+    """Returns prime factorization elements of x"""
+    div = 2
+    array = list()
+    while (div <= x):
+        if x % div == 0:
+            array.append(div)
+            x /= div
+        else:
+            div += 1
+
+    return array
 
 
 def minOperations(n):
-    """Calculates the minimum number of operations"""
-    prime_factors = []
-    minOp = 0
-
-    if n < 2 or type(n) != int:
-        return 0
-
-    while n % 2 == 0:
-        prime_factors.append(2)
-        n /= 2
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            prime_factors.append(i)
-            n /= i
-
-    if n > 2:
-        prime_factors.append(n)
-
-    for i in range(len(prime_factors)):
-        minOp += prime_factors[i]
-
-<<<<<<< HEAD
-    return int(minOp)
-=======
-    return int(minOp)
->>>>>>> d7a3cce259cf6a7f5e372e67a1f9878b3def409c
+    """Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file"""
+    min = 0
+    factors = [x for x in primeFactorization(n)]
+    occurences = {item: factors.count(item) for item in factors}
+    for k, v in occurences.items():
+        min += k * v
+    return min
