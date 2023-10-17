9757d7123162ce9b3d30f34097030e315d8948e4
muluante <muluante@outlook.com>
2023-10-17 01:11:48 -0700

    new update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,78 +1,88 @@
 #!/usr/bin/python3
-"""log parsing"""
-import sys
+'''A script for parsing HTTP request logs.
+'''
+import re
 
-file_size = 0
-l_200 = 0
-l_301 = 0
-l_400 = 0
-l_401 = 0
-l_403 = 0
-l_404 = 0
-l_405 = 0
-l_500 = 0
 
-for i, line in enumerate(sys.stdin, 1):
-    log_parts = line.split()
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-    if len(log_parts) != 9:
-        continue
+
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
+
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        status_c = int(log_parts[7])
-        file_size += int(line.split()[8])
-    except ValueError:
-        continue
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
-    if status_c == 200:
-        l_200 += 1
-    if status_c == 301:
-        l_301 += 1
-    if status_c == 400:
-        l_400 += 1
-    if status_c == 401:
-        l_401 += 1
-    if status_c == 403:
-        l_403 += 1
-    if status_c == 404:
-        l_404 += 1
-    if status_c == 405:
-        l_405 += 1
-    if status_c == 500:
-        l_500 += 1
 
-    if i % 10 == 0:
-        print(f"File size: {file_size}")
-        if l_200:
-            print(f"200: {l_200}")
-        if l_301:
-            print(f"301: {l_301}")
-        if l_400:
-            print(f"400: {l_400}")
-        if l_401:
-            print(f"401: {l_401}")
-        if l_403:
-            print(f"403: {l_403}")
-        if l_404:
-            print(f"404: {l_404}")
-        if l_405:
-            print(f"405: {l_405}")
-        if l_500:
-            print(f"500: {l_500}")
-
-print(f"File size: {file_size}")
-if l_200:
-    print(f"200: {l_200}")
-if l_301:
-    print(f"301: {l_301}")
-if l_400:
-    print(f"400: {l_400}")
-if l_401:
-    print(f"401: {l_401}")
-if l_403:
-    print(f"403: {l_403}")
-if l_404:
-    print(f"404: {l_404}")
-if l_405:
-    print(f"405: {l_405}")
-if l_500:
-    print(f"500: {l_500}")
+if __name__ == '__main__':
+    run()
